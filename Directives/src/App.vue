c7933de641c202b938c9388d446b61a59ee3c8a1
JaceRiehl <jace.riehl@gmail.com>
2018-05-03 13:34:44 -0600

    Added more attributes and modifiers to the directive to give a base for future directives

--- Directives/src/App.vue
+++ Directives/src/App.vue
@@ -12,7 +12,9 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
-                <p v-highlight:background="'red'">Color This</p>
+                <p v-highlight:background.delayed="'red'">Color This</p>
+                <p v-local-directive-highlight:background.blink="{mainColor: 'red', secondaryColor: 'green', delay: 500}">
+                    Color this too, but locally!</p>
             </div>
         </div>
     </div>
@@ -20,6 +22,39 @@
 
 <script>
     export default {
+        directives: {
+            'local-directive-highlight': {
+                bind(el,binding,vnode){
+                    var delay = 0;
+                    if(binding.modifiers['delayed']){
+                        delay = 3000;
+                    }
+                    if(binding.modifiers['blink']){
+                        let mainColor = binding.value.mainColor;
+                        let secondaryColor = binding.value.secondaryColor;
+                        let currentColor = mainColor;
+
+                        setTimeout(() => {
+                            setInterval(() => {
+                            currentColor == secondaryColor ? currentColor = mainColor : currentColor = secondaryColor;
+                            if(binding.arg == 'background') {
+                                el.style.backgroundColor = currentColor;
+                            } else {
+                                el.style.color = currentColor;
+                            }
+                            },binding.value.delay);
+                        },delay);
+                    }
+                    setTimeout(()=> {
+                        if(binding.arg == 'background') {
+                            el.style.backgroundColor = binding.value.mainColor;
+                        } else {
+                            el.style.color = binding.value.mainColor;
+                        }
+                    }, delay);
+                }
+            }
+        }
     }
 </script>
 
