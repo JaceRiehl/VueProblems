f0de23c00e22afcf423a18f166c0a9b631b44f28
JaceRiehl <jace.riehl@gmail.com>
2018-05-03 08:11:33 -0600

    Implemented an example of validating props and passing data back to parent through custom events

--- Communicating_Components/src/components/User.vue
+++ Communicating_Components/src/components/User.vue
@@ -3,11 +3,13 @@
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
         <button @click="changeName">Change my Name</button>
+        <p>My name is: {{name}}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
                 <!--the :name="name" makes it dynamic, so it will input the value of name instead of a string 'name'-->
-                <app-user-detail :name="name"></app-user-detail>
+                <!--this is where we listen to the event that was emitted from the child, which can be used to gather data-->
+                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -29,10 +31,11 @@
             },
         methods: {
            changeName: function(){
-               if(this.name == "Jace")
-                this.name = 'Not Jace';
-               else
-                   this.name = 'Jace';
+               this.name = "Not Jace"
+               // if(this.name == "Jace")
+               //  this.name = 'Not Jace';
+               // else
+               //     this.name = 'Jace';
            }
 
         },
--- Communicating_Components/src/components/UserDetail.vue
+++ Communicating_Components/src/components/UserDetail.vue
@@ -2,14 +2,37 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User Name: {{name}}</p>
+        <button @click="resetName">Reset Name</button>
+        <p>User Name: {{myName}}</p>
     </div>
 </template>
 
 <script>
 export default{
     //this property 'name' has to be the same as the parent.
-    props: ['name']
+    //by turning props into an object you can force the prop to have certain types
+    props: {
+        //this says that myName can only be a String or an Array
+        //myName: [String,Array]
+        //myName: String
+        //This can also be an object with certain types of properties
+        myName: {
+            type: String,
+            //This will make it so that this property has to be sent into this component through HTML
+            //required: true
+            //The default value
+            default: "You shouldnt see this"
+        }
+    },
+    methods: {
+        //This only resets the name in the child
+        //We need a custom event in order to tell the parent that the name has been changed
+        resetName: function(){
+            this.myName = "Jace";
+            //This emits a custom event that can be listened to by the parent using the component
+            this.$emit('nameWasReset', this.myName);
+        }
+    }
 }
 </script>
 
