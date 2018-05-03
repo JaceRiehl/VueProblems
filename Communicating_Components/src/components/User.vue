ce9ac6dafd600766110e152e700ca96123f2a399
JaceRiehl <jace.riehl@gmail.com>
2018-05-03 07:33:53 -0600

    Implemented an example of sending data from a parent component to a child component using props

---
+++ Communicating_Components/src/components/User.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <!--the :name="name" makes it dynamic, so it will input the value of name instead of a string 'name'-->
+                <app-user-detail :name="name"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data:
+            function() {
+                return {
+                    name: 'Jace'
+                };
+            },
+        methods: {
+           changeName: function(){
+               if(this.name == "Jace")
+                this.name = 'Not Jace';
+               else
+                   this.name = 'Jace';
+           }
+
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ Communicating_Components/src/components/UserDetail.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{name}}</p>
+    </div>
+</template>
+
+<script>
+export default{
+    //this property 'name' has to be the same as the parent.
+    props: ['name']
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
