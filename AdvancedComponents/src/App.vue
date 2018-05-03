3a11dd3fc90df99329e7158831161767c8553c46
JaceRiehl <jace.riehl@gmail.com>
2018-05-03 10:16:12 -0600

    Dynamically adding components, this is important

--- AdvancedComponents/src/App.vue
+++ AdvancedComponents/src/App.vue
@@ -2,10 +2,17 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               <app-quote>
-                   <h1 slot="title">{{quoteTitle}}</h1>
-                   <p>A Wonderful Quote!</p>
-               </app-quote>
+                <button @click="selectedComponent='app-quote'">Quote</button>
+                <button @click="selectedComponent='app-author'">Author</button>
+                <button @click="selectedComponent='app-new'">New</button>
+
+                <hr>
+                <!--this is how to dynamically add or change components-->
+                <component :is="selectedComponent"> Default </component>
+               <!--<app-quote>-->
+                   <!--<h1 slot="title">{{quoteTitle}}</h1>-->
+                   <!--<p>A Wonderful Quote!</p>-->
+               <!--</app-quote>-->
             </div>
         </div>
     </div>
@@ -13,14 +20,19 @@
 
 <script>
     import Quote from './components/Quote.vue'
+    import New from './components/New.vue'
+    import Author from './components/Author.vue'
     export default {
         data() {
             return{
-              quoteTitle: 'A Quote'
+                quoteTitle: 'A Quote',
+                selectedComponent: 'app-quote'
             }
         },
         components: {
-            'app-quote': Quote
+            'app-quote': Quote,
+            'app-author': Author,
+            'app-new': New
         }
     }
 </script>
