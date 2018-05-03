c5e262116bf023392c8d044e6f46e2ea8bb349d8
JaceRiehl <jace.riehl@gmail.com>
2018-05-03 09:57:00 -0600

    Added an example of slots in the AdvancedComponents folder

---
+++ AdvancedComponents/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <app-quote>
+                   <h1 slot="title">{{quoteTitle}}</h1>
+                   <p>A Wonderful Quote!</p>
+               </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue'
+    export default {
+        data() {
+            return{
+              quoteTitle: 'A Quote'
+            }
+        },
+        components: {
+            'app-quote': Quote
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ AdvancedComponents/src/Components/Quote.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <div class="title">
+        <slot name="title"></slot>
+        <!--this is a slot default, if nothing is put into them then they will just display the default information-->
+        <span style="color: #ccc"><slot name="subtitle">Default Slot</slot></span>
+    </div>
+    <div>
+        <!--unnamed alots are considered default slots and everything thats not titled will go in them-->
+        <slot></slot>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    div {
+        text-align: center;
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: auto;
+    }
+    h1 {
+        color: red;
+    }
+    .title {
+        font-style: italic;
+    }
+</style>
