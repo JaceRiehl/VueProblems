a7aa9e4cbd0673b58cbe795be85284fcf385a1a0
JaceRiehl <jace.riehl@gmail.com>
2018-05-04 12:11:04 -0600

    In HTTP folderin the main.js you can find an example of interceptors for requests and responses from http

--- Accessing_HTTP/src/main.js
+++ Accessing_HTTP/src/main.js
@@ -3,7 +3,25 @@ import VueResource from 'vue-resource'
 import App from './App.vue'
 
 Vue.use(VueResource);
+Vue.http.options.root = 'https://httppract.firebaseio.com/data.json';
+//This is an interceptor for a request and a response
+Vue.http.interceptors.push((request,next) => {
+    console.log(request);
+    //this intercepts the request
+    if(request.method == 'POST') {
+        request.method('PUT');
+    }
+    //this is the interceptor for the response
+    next(response => {
+      response.json = () => {
+        return {
+          messages: response.body 
+        }
+      }
+    });
+
+});
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
