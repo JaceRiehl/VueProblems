dfaea83086b13cf8c5678ef7d92a785c3c90b4c6
JaceRiehl <jace.riehl@gmail.com>
2018-05-04 12:56:07 -0600

    Added custom Actions for requests and responses in the Accessing\_HTTP folder

--- Accessing_HTTP/src/App.vue
+++ Accessing_HTTP/src/App.vue
@@ -13,6 +13,8 @@
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
                 <hr>
+                <input type="text" class="form-control" v-model="node">
+                <br><br>
                 <button class="btn btn-primary" @click="fetchData">Fetch Data</button>
                 <ul class="list-group" >
                     <li class="list-group-item" v-for="user in users">{{user.userName}} - {{user.email}}</li>
@@ -30,32 +32,57 @@
                     userName: '',
                     email: ''
                 },
-                users: []
+                users: [],
+                resource: {},
+                node: 'data'
             }
         },
         methods: {
             submit() {
                 //vue-resource posting data to the http
-                this.$http.post('https://httppract.firebaseio.com/data.json',this.userData)
-                    .then(response => {
-                    console.log(response);
-                }, error => {
-                        console.log(error);
-                    });
+                // this.$http.post('data.json',this.userData)
+                //     .then(response => {
+                //     console.log(response);
+                // }, error => {
+                //         console.log(error);
+                //     });
+                //this.resource.save({}, this.userData)
+                //We can use our custom action here instead
+                this.resource.saveAlt(this.userData);
             },
             fetchData() {
-                this.$http.get('https://httppract.firebaseio.com/data.json')
+                // this.$http.get('data.json')
+                //     .then(response => {
+                //         return response.json()
+                //     }) .then(data => {
+                //         const resultArray = [];
+                //         for(let key in data){
+                //             resultArray.push(data[key]);
+                //         }
+                //         this.users = resultArray;
+                //     }
+                // );
+                this.resource.getData({node: this.node})
                     .then(response => {
-                        return response.json()
-                    }) .then(data => {
-                        const resultArray = [];
-                        for(let key in data){
-                            resultArray.push(data[key]);
-                        }
-                        this.users = resultArray;
-                    }
-                );
+                                return response.json()
+                            }) .then(data => {
+                                const resultArray = [];
+                                for(let key in data){
+                                    resultArray.push(data[key]);
+                                }
+                                this.users = resultArray;
+                            }
+                        );
             }
+        },
+        created() {
+            //custom action to be performed
+            const customActions = {
+                saveAlt: {method: 'POST', url: 'alternative.json'},
+                getData: {method: 'GET'}
+            }
+            //this could be more centralized to share across multiple components
+            this.resource = this.$resource('{node}.json', {}, customActions)
         }
     }
 </script>
--- Accessing_HTTP/src/main.js
+++ Accessing_HTTP/src/main.js
@@ -3,19 +3,19 @@ import VueResource from 'vue-resource'
 import App from './App.vue'
 
 Vue.use(VueResource);
-Vue.http.options.root = 'https://httppract.firebaseio.com/data.json';
+Vue.http.options.root = 'https://httppract.firebaseio.com/';
 //This is an interceptor for a request and a response
 Vue.http.interceptors.push((request,next) => {
     console.log(request);
     //this intercepts the request
     if(request.method == 'POST') {
-        request.method('PUT');
+        request.method = 'PUT';
     }
     //this is the interceptor for the response
     next(response => {
       response.json = () => {
         return {
-          messages: response.body 
+          messages: response.body
         }
       }
     });
