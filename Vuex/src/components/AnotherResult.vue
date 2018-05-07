a834ac386e4bf9bb4160b02c27b66f2693fe27de
JaceRiehl <jace.riehl@gmail.com>
2018-05-07 11:54:42 -0600

    Added examples of using getters with vuex and mapping the getters to properties in the AnotherResult file

---
+++ Vuex/src/components/AnotherResult.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>Counter is: {{ doubleCounter }}</p>
+        <hr>
+        <p>Number of Clicks: {{ stringCounter }}</p>
+    </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex'
+    export default {
+        //this will create properties for the getters and map the getters to the getters in the store
+        computed: {
+            //The triple dot is an es6 property that tells javascript to pull out the methods from the object and
+            //create seperate key value pairs
+            ...mapGetters([
+                'doubleCounter',
+                'stringCounter'
+            ])
+        }
+    }
+</script>
