2e2a1b2d642936d6e4bc4eb43f3705e71aa77ee0
JaceRiehl <jace.riehl@gmail.com>
2018-05-07 09:35:31 -0600

    Added multiple router views in App.vue and added example of redirection (specific and catch all)

--- Routing/src/Routes.js
+++ Routing/src/Routes.js
@@ -3,12 +3,23 @@ import Home from './components/Home.vue'
 import UserStart from './components/user/UserStart.vue'
 import UserDetail from './components/user/UserDetail.vue'
 import UserEdit from './components/user/UserEdit.vue'
+import Header from './components/Header.vue'
 export const routes = [
-    {path: '', component: Home},
-    //id is a flexible element
-    {path: '/user', component: User, children: [
+    //this is the default path
+    {path: '', name: 'home', components: {
+        default: Home,
+        'header-top': Header
+        }},
+    //this sets up the user path
+    {path: '/user', components: {
+        default: User,
+        'header-bottom': Header}, children: [
             {path: '', component: UserStart, name: 'home'},
+            //id is a flexible element
             {path:':id', component: UserDetail},
             {path:':id/edit', component: UserEdit, name: 'userEdit'}
-        ]}
+        ]},
+    //this is a specific redirection
+    {path: '/redirect-me', redirect: {name: 'home'}},
+    {path: '*', redirect: '/'}
 ];
